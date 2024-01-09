a2414761592167d6a1bf8f0762ee147f383c7672
ping <ping.jia@devops.com>
2024-01-09 21:13:53 +0800

    first commit

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,5 @@
+//static void main(String[] args) {
+//  println "Hello world!"
+//}
+
+
---
+++ src/main/groovy/MyLib.groovy
@@ -0,0 +1,7 @@
+def getTime() {
+    return new Date().toString()
+}
+
+def getJenkinsHome() {
+    return "${JENKINS_HOME}";
+}
